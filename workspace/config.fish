506776003f909b85b698711a63a2b935579beef1
Ciao Chung <ciao0958@gmail.com>
2021-06-28 10:39:17 +0800

    chore: 加入個人常用設定、套件

---
+++ workspace/config.fish
@@ -0,0 +1,10 @@
+alias la="php artisan"
+alias clip="xclip -i -selection clipboard"
+alias ..="cd ../"
+alias ...="cd ../../"
+alias ....="cd ../../../"
+alias .....="cd ../../../../"
+alias phpu="./vendor/bin/phpunit"
+
+alias sshsample="ssh -o StrictHostKeyChecking=no username@host.com"
+set -g fish_prompt_pwd_dir_length 0
